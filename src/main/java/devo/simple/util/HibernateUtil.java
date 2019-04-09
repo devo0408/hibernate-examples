3776366993fbe100a8b46271a880d16a25b4d802
vovkd <Denys.Vovk@genios.de>
2019-04-09 11:36:51 +0300

    various changes

--- src/main/java/devo/simple/util/HibernateUtil.java
+++ src/main/java/devo/simple/util/HibernateUtil.java
@@ -9,42 +9,26 @@ import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 
-    private static StandardServiceRegistry registry;
     private static SessionFactory sessionFactory;
 
     public static SessionFactory getSessionFactory() {
-        try {
-            Configuration configuration = new Configuration();
-            ServiceRegistry serviceRegistry
-                    = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-            configuration.addAnnotatedClass(devo.simple.data.Student.class);
-            return configuration.buildSessionFactory(serviceRegistry);
-        }catch(Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("There is issue in hibernate util");
+
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                ServiceRegistry serviceRegistry
+                        = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                configuration.addAnnotatedClass(devo.simple.data.Student.class);
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("There is issue in hibernate util");
+            }
         }
 
-//        if (sessionFactory == null) {
-//            try {
-//                registry = new StandardServiceRegistryBuilder().configure("hibernate.properties").build();
-//                MetadataSources sources = new MetadataSources(registry);
-//                Metadata metadata = sources.getMetadataBuilder().build();
-//                sessionFactory = metadata.getSessionFactoryBuilder().build();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                if (registry != null) {
-//                    StandardServiceRegistryBuilder.destroy(registry);
-//                }
-//            }
-//        }
-//        return sessionFactory;
+        return sessionFactory;
     }
-//    public static void shutdown() {
-//        if (registry != null) {
-//            StandardServiceRegistryBuilder.destroy(registry);
-//        }
-//    }
 
     // https://dzone.com/articles/hibernate-5-xml-configuration-example
     // https://proselyte.net/tutorials/hibernate-tutorial/configure/
